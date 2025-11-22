156fa802647cf2b62e81382fe3eb36971f4976fb
Lab <naylorelectronics@gmail.com>
2025-11-22 01:21:04 +0000

    Add files via upload

--- debian/default/start.sh
+++ debian/default/start.sh
@@ -6,7 +6,14 @@ sudo service tor start
 sudo systemctl status tor
 
 sudo proxychains4 apt install wget -y
+
+cd ~/
+mkdir Lab
+mkdir Lab/Internet
+
 sudo proxychains4 wget "https://www.torproject.org/dist/torbrowser/15.0.2/tor-browser-linux-x86_64-15.0.2.tar.xz"
+sudo tar -xzvf tor-browser-linux-x86_64-15.0.2.tar.xz 
+cd tor-browser-linux-x86_64-15.0.2
 
 sudo proxychains4 apt install neovim -y 
 
