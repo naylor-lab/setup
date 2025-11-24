6827049fa05b667676a7c827cd678a8eaba71f63
Lab <naylor-laboratory@tutamail.com>
2025-11-24 16:59:47 -0300

    Update start.sh

--- debian/default/start.sh
+++ debian/default/start.sh
@@ -3,22 +3,23 @@ sudo apt install proxychains-ng tor -y
 #socks4 127.0.0.1 9050
 #
 
+sudo proxychains4 apt install wget neovim -y
 
-sudo service tor start
-sudo systemctl status tor
-
-sudo proxychains4 apt install wget -y
-
+#Create Lab:
 cd ~/
 mkdir Lab
 mkdir Lab/Internet
 cd Lab/Internet
 
-sudo proxychains4 wget "https://www.torproject.org/dist/torbrowser/15.0.2/tor-browser-linux-x86_64-15.0.2.tar.xz"
-sudo tar -xzvf tor-browser-linux-x86_64-15.0.2.tar.xz 
+#Init Tor Lab:
+sudo proxychains4 wget  "https://www.torproject.org/dist/torbrowser/15.0.2/tor-browser-linux-x86_64-15.0.2.tar.xz"
+
+tar -xf tor-browser-linux-x86_64-15.0.2.tar.xz 
+
 rm -rf tor-browser-linux-x86_64-15.0.2.tar.xz 
 
-sudo proxychains4 apt install neovim -y 
+sudo service tor start
+sudo systemctl status tor
 
 
 
