b3ca4f9ba2ced5b98b35b97afbc7c0da3f7f578d
Lab <naylor-laboratory@tutamail.com>
2025-11-25 18:13:41 +0000

    Update start.sh

--- debian/default/start.sh
+++ debian/default/start.sh
@@ -10,14 +10,17 @@ mkdir Lab
 mkdir Lab/Internet
 cd Lab/Internet
 
-#Init Tor Lab:
+
+#Create Tor Lab:
 sudo proxychains4 wget  "https://www.torproject.org/dist/torbrowser/15.0.2/tor-browser-linux-x86_64-15.0.2.tar.xz"
 
+
 tar -xf tor-browser-linux-x86_64-15.0.2.tar.xz 
-
 rm -rf tor-browser-linux-x86_64-15.0.2.tar.xz 
-
 sudo chmod +x tor-browser
+
+
+
 
 
 
