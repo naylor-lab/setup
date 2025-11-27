d6295666fb72281211069b2af722464222da8cc1
Lab <naylor-laboratory@tutamail.com>
2025-11-27 16:50:41 -0300

    Update and rename termux/player/start.sh to termux/default/desktop/environment/xfce/start.sh

---
+++ termux/default/desktop/environment/xfce/start.sh
@@ -0,0 +1,10 @@
+pkg update
+pkg install x11-repo
+pkg install termux-x11-nightly
+pkg install pulseaudio
+pkg install wget
+
+pkg install xfce4
+pkg install tur-repo
+pkg install firefox
+pkg install neovim 
