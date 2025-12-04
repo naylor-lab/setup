a06aeb3a7b81879a9d1d52ef0110fdf2cc58d7fb
Lab <naylor-laboratory@tutamail.com>
2025-12-04 15:10:19 -0300

    Install wget and download Thunderbird
    Added wget installation and updated Thunderbird download link.

--- default/install.sh
+++ default/install.sh
@@ -1,3 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get wget
 sudo apt-get install keepassx
+
+wget "https://download.mozilla.org/?product=thunderbird-145.0-SSL&os=linux64&lang=en-US"
